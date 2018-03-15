26d5085e071c1de8d8b7fc8891fbc37465c9c8d7
jijin <ajream.gd@gmail.com>
2018-03-15 17:21:47 +0800

    add user validates.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  attr_accessor :password, :password_confirmation
+
+  validates_presence_of :email, message: "邮箱不能为空！"
+  validates :email, uniqueness: true
+  validates_presence_of :password, message: "密码不能为空！"
+  validates_presence_of :password_confirmation, message: "密码确认不能为空！"
+  validates_confirmation_of :password, message: "密码不一致。"
 end
